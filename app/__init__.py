0f0c2a372f1864bcacfe0a380847c81c6c83a126
AquaUseful <l.vladik.m@gmail.com>
2021-01-24 00:36:08 +0300

    Fix initialization and start

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import quart
 import hypercorn
 import os
+from app.utils import blueprints
 
 app = quart.Quart(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
+blueprints.register_blueprints(app)
--- app/__main__.py
+++ app/__main__.py
@@ -8,3 +8,30 @@ try:
     from app import config
 except ImportError:
     pass
+
+shutdown_event = asyncio.Event()
+
+
+@app.after_serving
+async def shutdown():
+    pass
+
+
+def signal_handler():
+    shutdown_event.set()
+
+
+hypercorn_cfg = hypercorn.Config()
+
+try:
+    hypercorn_cfg.bind = [f"{config.HOST}:{config.PORT}",
+                          f"[{config.HOST_IPV6}]:{config.PORT}"]
+except NameError:
+    hypercorn_cfg.bind = [f"{os.environ['HOST']}:{os.environ['PORT']}",
+                          f"[{os.environ['HOST_IPV6']}]:{os.environ['PORT']}"]
+
+
+loop = asyncio.get_event_loop()
+loop.add_signal_handler(signal.SIGINT, signal_handler)
+loop.run_until_complete(hypercorn.asyncio.serve(
+    app, hypercorn_cfg, shutdown_trigger=shutdown_event.wait))
