8db15154793d4cb1a1218883b85a9e3f362cff08
AquaUseful <l.vladik.m@gmail.com>
2021-01-24 13:17:11 +0300

    Load all blueprints

--- app/utils/blueprints.py
+++ app/utils/blueprints.py
@@ -1,6 +1,7 @@
-from app.modules import users, tokens
+from app.modules import users, tokens, chats
 
 
 def register_blueprints(app):
     app.register_blueprint(users.blueprint)
     app.register_blueprint(tokens.blueprint)
+    app.register_blueprint(chats.blueprint)
