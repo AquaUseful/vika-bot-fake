5e7fc2e9923df162cb00c00d18c4a522c5a934fc
AquaUseful <l.vladik.m@gmail.com>
2021-01-24 13:16:46 +0300

    Add users module (fake user info)

---
+++ app/modules/users.py
@@ -0,0 +1,38 @@
+import quart
+import typing
+from app.utils import utils
+from app.utils import data as data_utils
+from app.utils import decorators
+
+try:
+    from app import config
+except ImportError:
+    pass
+
+blueprint = quart.Blueprint("users", __name__)
+
+
+@blueprint.route("/api/users/info", methods=["POST"])
+@decorators.req_fields({"token": str, "ids": typing.Iterable})
+@decorators.token_verify
+async def get_user():
+    req_json = await quart.request.json
+    try:
+        users = await data_utils.get_users_info(*req_json["ids"])
+    except ValueError as exc:
+        await quart.abort(400, exc)
+    user_dict = {user.id: {item[0]: item[1] for item in vars(user).items() if utils.is_jsonable(item[1])}
+                 for user in users}
+    return quart.jsonify(user_dict)
+
+
+@blueprint.route("/api/users/<int:user_id>/photo", methods=["GET"])
+async def photo(user_id):
+    photo = await data_utils.get_last_photo(user_id)
+    if photo is None:
+        await quart.abort(404)
+    resp = await quart.make_response(photo)
+    resp.headers.set("Content-Type", "image/jpeg")
+    resp.headers.set("Content-Disposition", "attachment",
+                     filename=f"{user_id}.jpeg")
+    return resp
